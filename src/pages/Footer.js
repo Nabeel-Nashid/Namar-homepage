6907a0f72cc972f9c690b83da3dade901bb62d01
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-08-04 16:09:25 +0530

    04/08 4-09

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -12,48 +12,48 @@ function Footer() {
     <div className="w-full md:h-[400px] h-full bg-[#e7f0f8] flex flex-col justify-center">
       <div className="grid grid-cols-2 ml-7 mt-7 md:mt-0 md:ml-0 md:flex flex-row md:justify-evenly text-[#162c53]">
         <div className="flex flex-col ">
-          <h1 className="text-2xl  font-semibold mb-3">Quick Links</h1>
+          <h1 className="md:text-2xl text-lg  font-semibold mb-3">Quick Links</h1>
           <Link to="home" smooth="true" duration={500}>
-          <h1 className="mb-2 font-medium cursor-pointer">Home</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs cursor-pointer">Home</h1>
           </Link>
           <Link to="about" smooth="true" duration={500}>
-          <h1 className="mb-2 font-medium cursor-pointer">About Us</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs cursor-pointer">About Us</h1>
           </Link>
           <Link to="contact" smooth="true" duration={500}>
-          <h1 className="mb-2 font-medium cursor-pointer">Contact US</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs cursor-pointer">Contact US</h1>
           </Link>
           <Link to="careers" smooth="true" duration={500}>
-          <h1 className="mb-2 font-medium cursor-pointer">Careers</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs cursor-pointer">Careers</h1>
           </Link>
         </div>
         <div className="flex flex-col">
-          <h1 className="text-2xl  font-medium mb-3">Services</h1>
-          <h1 className="mb-2 font-medium">Food Stuff</h1>
-          <h1 className="mb-2 font-medium">Building Materail</h1>
-          <h1 className="mb-2 font-medium">Import & Export</h1>
-          <h1 className="mb-2 font-medium">General Contracting</h1>
+          <h1 className="md:text-2xl text-lg  font-medium mb-3">Services</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs">Food Stuff</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs">Building Materail</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs">Import & Export</h1>
+          <h1 className="mb-2 font-medium md:text-base text-xs">General Contracting</h1>
         </div>
         <div className="flex flex-col">
-          <h1 className="text-2xl  font-medium mb-3">Contact US</h1>
+          <h1 className="md:text-2xl text-lg font-medium mb-3">Contact US</h1>
           <div className="flex items-center mb-3">
-            <img src={location} className="w-6 h-5" />
-            <h1 className="font-medium">311,C 496,Suhar,Mascut</h1>
+            <img src={location} className="md:w-6 w-4 md:h-5 h-3" />
+            <h1 className="font-medium md:text-base text-xs">311,C 496,Suhar,Mascut</h1>
           </div>
           <div className="flex items-center mb-3">
-            <img src={phone} className="w-6 h-5" />
-            <h1 className="font-medium">+96826759366</h1>
+            <img src={phone} className="md:w-6 w-4 md:h-5 h-3" />
+            <h1 className="font-medium md:text-base text-xs">+96826759366</h1>
           </div>
           <div className="flex items-center mb-3">
-            <img src={mail} className="w-6 h-5 mr-2 flex-shrink-0" />
-            <h1 className="font-medium">info@namaralghubairaa.com</h1>
+            <img src={mail} className="md:w-6 w-4 md:h-5 h-3 mr-2 " />
+            <h1 className="font-medium md:text-base text-xs">info@namaralghubairaa.com</h1>
           </div>
         </div>
         <div className="flex flex-col">
-          <h1 className="text-2xl  font-medium mb-3">Follow US</h1>
+          <h1 className="md:text-2xl text-lg font-medium mb-3">Follow US</h1>
           <div className="flex">
-            <img src={fb} className="w-6 h-6 mr-3" />
-            <img src={insta} className="w-6 h-6 mr-3" />
-            <img src={twitter} className="w-6 h-6 mr-3" />
+            <img src={fb} className="md:w-6 w-4 md:h-6 h-4 mr-3" />
+            <img src={insta} className="md:w-6 w-4 md:h-6 h-4 mr-3" />
+            <img src={twitter} className="md:w-6 w-4 md:h-6 h-4 mr-3" />
           </div>
         </div>
       </div>
